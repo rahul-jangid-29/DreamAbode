1ef9b421c6d2630f7e6b398282598da2ebde01ab
rahul jangid <rahuljangid4380@gmail.com>
2025-01-07 00:57:56 +0530

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/DreamAbode/',  // Make sure this matches your repository name
+  build: {
+    outDir: 'dist', // Ensure this matches the output directory
+  }
 })
